36ff6bb1c1649f71b3ef0d293ed8bbcb61964b3a
Caleb French <cmfrench808@gmail.com>
2026-03-02 09:50:49 -0500

    Venv Setup file working front+back end

--- Scripts/venvSetup.sh
+++ Scripts/venvSetup.sh
@@ -4,22 +4,26 @@
 PATH_TO_ENVIROMENTS=pythonEnviroments
 BACKEND_VENV_NAME=backEnd
 FRONTEND_VENV_NAME=frontEnd
-BE_REQUIREMENTS=backendRequirements.txt
-FE_REQUIREMENTS=frontendRequirements.txt
+BE_REQUIREMENTS=linuxBackReqs.txt
+FE_REQUIREMENTS=linuxFrontReqs.txt
 
 ## move into our directory
 cd ${PATH_TO_ENVIROMENTS}
 
 ## delete existing virtual enviroments
+echo "removing existing Venv"
 rm -r ${BACKEND_VENV_NAME}
 rm -r ${FRONTEND_VENV_NAME}
 
 ## create our virual enviroments, and install required libraries wihtin them.
-python -m venv ${FRONTEND_VENV_NAME}
-${FRONTEND_VENV_NAME}/Scripts/activate
+
+echo "Installing Frontend Venv"
+python3.11 -m venv ${FRONTEND_VENV_NAME}
+source ${FRONTEND_VENV_NAME}/bin/activate
 pip install -r ${FE_REQUIREMENTS}
 
-python -m venv ${BACKEND_VENV_NAME}
-${BACKEND_VENV_NAME}/Scripts/activate
+echo "Installing Backend Venv"
+python3.11 -m venv ${BACKEND_VENV_NAME}
+source ${BACKEND_VENV_NAME}/bin/activate
 pip install -r ${BE_REQUIREMENTS}
 
