d105f2422e77eeffb8cc3488d588eac479d4b1b3
Caleb <cmfrench808@gmail.com>
2026-04-12 19:53:22 -0400

    update to bash to make them work from any directory

--- movie/docker/run.sh
+++ movie/docker/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd "$(dirname "$(readlink -f "$0")")" # Changes working directory to the directory of the file (allows bash to be called from anywhere)
 docker run --env-file .env --rm -p 22061:6161 --name movie_mood movierecommend 
